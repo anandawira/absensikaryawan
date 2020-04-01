9a340496eae411afa062af96327f3fb691951cf7
AnandaWira <anandawira754@gmail.com>
2020-04-01 19:39:05 +0700

    login form added

---
+++ dashboard.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>
+        <div class="content">
+
+        </div>
+        <aside>
+            
+        </aside>
+    </main>
+    <footer>
+
+    </footer>
+    
+</body>
+</html>
